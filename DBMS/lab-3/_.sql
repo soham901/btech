bb273a610e006e51acb2fac0b08a9c7b750603a0
soham901 <you@example.com>
2024-07-27 11:54:48 +0200

    updates

---
+++ DBMS/lab-3/_.sql
@@ -0,0 +1,9 @@
+CREATE TABLE STUDENT (RollNo INT, Name VARCHAR(50), Birthdate DATE, SPI DECIMAL(2,2), City VARCHAR(50), Backlog INT, Branch VARCHAR(50));
+
+
+INSERT INTO STUDENT VALUES (101, 'Keyur', '1-5-92', 8.5, 'Rajkot', 2, 'CE');
+INSERT INTO STUDENT VALUES (102, 'Hardik', '15-2-94', 9.0, 'Ahmedabad', 0, 'CE');
+INSERT INTO STUDENT VALUES (103, 'Kajal', '14-3-96', 10.0, 'Baroda', 0, 'IT');
+INSERT INTO STUDENT VALUES (104, 'Bhoomi', '23-6-95', 8.9, 'Ahmedabad', 1, 'ICT');
+INSERT INTO STUDENT VALUES (105, 'Harmit', '15-2-94', 9.8, 'Rajkot', 1, 'IT');
+INSERT INTO STUDENT VALUES (106, 'Jay', '15-2-94', 7.9, 'Rajkot', 2, 'CE');
