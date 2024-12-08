46378b1464f2a41d7aa6fbb56507081cd838ffaf
Soham <sagathiyasoham12345@gmail.com>
2024-12-08 07:21:45 +0530

    init: server of the WT mini project

---
+++ wt-2/project/server/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'it Works' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
